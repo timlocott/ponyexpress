914d4fc15976899a3c32a1188fb00d4eda616117
timlocott <tsam.cottrell@gmail.com>
2024-04-08 18:18:04 -0600

    CORS wildcard methods

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], # change this as appropriate for your setup
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
