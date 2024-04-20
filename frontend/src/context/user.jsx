87e1df96f276f4491aa975367e6366e5776a057d
timlocott <tsam.cottrell@gmail.com>
2024-04-20 10:50:31 -0600

    Updated Auth to use api with correct url

--- frontend/src/context/user.jsx
+++ frontend/src/context/user.jsx
@@ -4,33 +4,28 @@ import { createContext, useContext } from "react";
 import { useQuery  } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./auth";
+import { useApi } from "../hooks";
 
 const UserContext = createContext();
 
 function UserProvider({ children }) {
     const { isLoggedIn, logout, token } = useAuth();
     const navigate = useNavigate();
+    const api = useApi();
 
     const { data } = useQuery({
         queryKey: ["users", token],
         enabled: isLoggedIn,
         staleTime: Infinity,
         queryFn: () => (
-            fetch(
-                "http://127.0.0.1:8000/users/me",
-                {
-                    headers: {
-                        "Authorization": "Bearer " + token,
-                        "Content-Type": "application/json",
-                    },
-                },
-            ).then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    logout();
-                    navigate("/login");
-                }
+            api.get("/users/me")
+            .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                logout();
+                navigate("/login");
+            }
             })
         ),
     });
